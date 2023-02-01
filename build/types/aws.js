03822118df7a7159e5e7ba6e6e7418aa61b41cb3
farrisismatiteach <farris.ismati.teach@gmail.com>
2023-02-01 10:16:09 -0500

    table setup complete

--- build/types/aws.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.AWSRegions = void 0;
-var AWSRegions;
-(function (AWSRegions) {
-    AWSRegions["US_EAST_1"] = "us-east-1";
-})(AWSRegions = exports.AWSRegions || (exports.AWSRegions = {}));
